4b7bc0814dc576da65f5b3437d94a5204b1f29f0
Harkame <louisr.daviaud@gmail.com>
2017-10-29 17:32:52 +0100

    Clear

--- src/services/tchat/tchat.h
+++
@@ -1,8 +0,0 @@
-#ifndef TCHAT_H
-#define TCHAT_H
-
-#include "../../server/server.h"
-
-void* tchat_handler(void* p_client_number);
-
-#endif
--- src/services/transfer/transfer.h
+++
@@ -1,8 +0,0 @@
-#ifndef TRANSFER_H
-#define TRANSFER_H
-
-#include "../../server/server.h"
-
-void* transfer_handler(void* p_client_number);
-
-#endif
